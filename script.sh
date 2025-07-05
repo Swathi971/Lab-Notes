400c6b79b6ca028b92228058a7d217f2e685438b
root <root@ip-11-0-23-165.ec2.internal>
2025-07-05 01:44:18 +0000

    script.sh is added

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+#This works only on Linux or redhat 
+sudo wget -O /etc/yum.repos.d/jenkins.repo \
+       	https://pkg.jenkins.io/redhat/jenkins.repo 
+sudo rpm --import https://pkg.jenkins.io/redhat/jenkins.io-2023.key
+sudo yum upgrade 
+# Add required dependencies for the jenkins package 
+sudo yum install fontconfig java-17 -y 
+sudo yum install jenkins –y 
+sudo systemctl daemon-reload 
+# Start Jenkins
+sudo systemctl enable jenkins 
+sudo systemctl start jenkins 
+sudo systemctl status jenkins 
