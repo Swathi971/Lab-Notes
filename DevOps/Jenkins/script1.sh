4d7f747875ceb15c9b0b4dde842aec2bc89cd47a
Swathi971 <swathimayya971@gmail.com>
2025-07-12 13:18:26 +0530

    Update script1.sh

--- DevOps/Jenkins/script1.sh
+++ DevOps/Jenkins/script1.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
 
+# Add Jenkins key and repo
+sudo mkdir -p /etc/apt/keyrings
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/" | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# Update package list
+sudo apt-get update
+
+# Install Jenkins
+sudo apt-get install -y jenkins
+
+# Install Java (OpenJDK 17)
+sudo apt install -y fontconfig openjdk-17-jdk
+
+# Verify Java version
+java -version
+
+# Start Jenkins service
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+# Check Jenkins status
+sudo systemctl status jenkins
